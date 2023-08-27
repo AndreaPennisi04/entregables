b2766169b213529d32517ae92b72cce48f0810bf
AndreaPennisi <andreapennisi04@gmail.com>
2023-08-27 15:20:57 +0100

    implementacion logger

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
+
 dotenv.config({
-  path: `.env.${process.env.NODE_ENV || "dev"}.local`,
+  path: `.env.${process.env.NODE_ENV || "desarrollo"}.local`,
 });
 
 const {
---
+++ src/utils/logger.js
@@ -0,0 +1,44 @@
+import winston from "winston";
+import config from "../config/config.js";
+
+const { LOG_LEVEL_CONSOLE, LOG_LEVEL_FILE } = config;
+
+const customLevelsOptions = {
+  levels: {
+    fatal: 0,
+    error: 1,
+    warning: 2,
+    info: 3,
+    http: 4,
+    debug: 5,
+  },
+  colors: {
+    fatal: "brightRed",
+    error: "red",
+    warning: "yellow",
+    info: "blue",
+    http: "green",
+    debug: "white",
+  },
+};
+
+const logger = winston.createLogger({
+  levels: customLevelsOptions.levels,
+  transports: [
+    new winston.transports.Console({
+      level: LOG_LEVEL_CONSOLE,
+      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+    }),
+    new winston.transports.File({ filename: "./errors.log", level: LOG_LEVEL_FILE }),
+  ],
+});
+
+export const addLogger = (req, res, next) => {
+  req.logger = logger;
+  req.logger.http(`${req.method} en ${req.url} - ${new Date().toLocaleTimeString()}`);
+  next();
+};
+
+export const getLogger = () => {
+  return logger;
+};
